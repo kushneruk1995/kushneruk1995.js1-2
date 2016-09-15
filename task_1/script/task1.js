53615ca15d887f2ac9e06c5f99731f4bda3804ce
kushneruk.github.io <kushneruk_1995@mail.ru>
2016-09-15 21:38:08 +0300

    changed codestyle

--- task_1/script/task1.js
+++ task_1/script/task1.js
@@ -1,28 +1,33 @@
-    var number=prompt("Введіть число, яке необхідно піднести до степіня");
-    console.log("число=",number);
-    var pow=prompt("Введіть ступінь, до якого необхідно піднести число");
-    console.log("степінь=",pow);
-    var result;
+var number = prompt("Введіть число, яке необхідно піднести до степіня");
+console.log("число=", number);
+var pow = prompt("Введіть ступінь, до якого необхідно піднести число");
+console.log("степінь=", pow);
+var result;//Змінна, в яку буде записуватись результат
 
-function exponentiation (number,pow) {
-	
-	if (pow>0) {
-	result=number;
+function exponentiation(number, pow) {
+  if (pow > 0) {
+    result = number;
+
     for (i = 0; i < pow-1; i++) {
-	     result=number*result;
+	  result = number*result;
     }
+
     console.log(result);
-} else if (pow==0) {
-	result=1;
-	console.log(result);
-} else if (pow<0) {
-	result=1/(number);
-	for (i = -1; i > pow; i--) {
-	    result=result/number;
+
+  } else if (pow == 0) {
+	  result=1;
+	  console.log(result);
+
+  } else if (pow < 0) {
+	  result=1/(number);
+
+	  for (i = -1; i > pow; i--) {
+	    result = result/number;
+      }
+
+      console.log(result);
     }
-    console.log(result);
-}
-}
+  }
 
 exponentiation(number,pow);
 
--- task_2/script/task2.js
+++ task_2/script/task2.js
@@ -1,20 +1,25 @@
-function enterName () {
-    var arr = new Array(5);
-    for (i = 0; i < 5; i++) {
-        arr[i] = prompt("Введіть ім'я");
-    }
-    var userName=prompt("Введіть ім'я користувача");
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i]==userName) {
-           var validName=userName;
-        } 
-    }
-
-    if (validName !=undefined) {
-        alert(validName+", ви успійшло ввійшли в систему");
-    } else {
-        alert("Введене ім'я недійсне");
-    }
+function enterName() {
+  //створюємо масив з 5 елементів
+  var arr = new Array(5);
+  //Циклом заповнюємо масив іменами
+  for (i = 0; i < arr.length; i++) {
+    arr[i] = prompt("Введіть ім'я");
+  }
+  
+  var userName = prompt("Введіть ім'я користувача");
+  //Перевірка на відповідність введеного ім'я користувача з іменами в масиві
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == userName) {
+      //Якщо ім'я в масиві співпадає з іменем користувача, воно записується у змінну validName
+      var validName = userName;
+    } 
+  }
+  //Якщо ім'я користувача співпало, відповідно змінна validName не буде undefined, в іншому випадку введене ім'я недійсне
+    if (validName != undefined) {
+    alert(validName + ", ви успійшло ввійшли в систему");
+  } else {
+    alert("Введене ім'я недійсне");
+  }
 }
 enterName();
 
